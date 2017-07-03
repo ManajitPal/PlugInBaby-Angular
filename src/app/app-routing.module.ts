7abe5d827345c03eda38aa731e378dd455ad290a
ManajitPal <manajitpal95@gmail.com>
2017-07-04 00:25:36 +0530

    Initial Upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FanComponent } from './fan/fan.component';
+import { EmployerComponent } from './employer/employer.component';
+import { ArtistComponent } from './artist/artist.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'employer',
+    component: EmployerComponent
+  },
+  {
+    path: 'artist',
+    component: ArtistComponent
+  },
+  {
+    path: 'fan',
+    component: FanComponent
   }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmployerComponent } from './employer/employer.component';
+import { ArtistComponent } from './artist/artist.component';
+import { FanComponent } from './fan/fan.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmployerComponent,
+    ArtistComponent,
+    FanComponent,
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
