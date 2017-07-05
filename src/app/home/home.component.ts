89d390f49d3e45cc1f07b55af082da66c1c32342
ManajitPal <manajitpal95@gmail.com>
2017-07-06 00:26:09 +0530

    homepage Second screen update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
+  host: {
+        '(window:scroll)': 'updateHeader($event)'
+    }
 })
 export class HomeComponent implements OnInit {
+  
+    isScrolled = false;
+    currPos: Number = 0;
+    startPos: Number = 0;
+    changePos: Number = 100;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  updateHeader(evt) {
+        this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+        if(this.currPos >= this.changePos ) {
+            this.isScrolled = true;
+        } else {
+            this.isScrolled = false;
+        }
+    }
+    
 
 }
